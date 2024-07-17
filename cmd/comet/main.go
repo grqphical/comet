fe57b20aa897dd89ce9594ae4ae5c36dfceaffb2
grqphical <95062977+grqphical@users.noreply.github.com>
2024-07-16 21:23:00 -0700

    added config loader

--- cmd/comet/main.go
+++ cmd/comet/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"comet/internal/config"
+	"fmt"
+	"os"
+)
 
+func LogError(err error) {
+	fmt.Printf("ERROR: %s\n", err)
+	os.Exit(1)
+}
+
+func main() {
+	err := config.ReadConfig()
+	if err != nil {
+		LogError(err)
+	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"errors"
+
+	"github.com/spf13/viper"
+)
+
+type Backend struct {
+	Address        string `mapstructure:"address"`
+	HealthEndpoint string `mapstructure:"health_endpoint"`
+}
+
+func ReadConfig() error {
+	viper.SetConfigName("comet")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return errors.New("no comet.toml file found")
+		} else {
+			return err
+		}
+	}
+
+	return nil
+}
