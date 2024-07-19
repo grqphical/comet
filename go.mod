71f591c358d463d1ac4cebabf64bc01975d03b58
grqphical <95062977+grqphical@users.noreply.github.com>
2024-07-18 17:26:14 -0700

    added pretty logging

--- go.mod
+++ go.mod
@@ -4,9 +4,12 @@ go 1.22.5
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/fatih/color v1.17.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,15 +1,105 @@
 package logging
 
 import (
-	"comet/internal/config"
+	"bytes"
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"os"
+	"sync"
+	"time"
 
+	"github.com/fatih/color"
 	"github.com/spf13/viper"
 )
+
+type PrettyPrintHandlerOptions struct {
+	Level slog.Leveler
+}
+
+type PrettyPrintHandler struct {
+	mu   *sync.Mutex
+	out  io.Writer
+	opts PrettyPrintHandlerOptions
+}
+
+func NewPrettyPrintHandler(out io.Writer, opts *PrettyPrintHandlerOptions) *PrettyPrintHandler {
+	h := &PrettyPrintHandler{
+		out: out,
+		mu:  &sync.Mutex{},
+	}
+
+	if opts != nil {
+		h.opts = *opts
+	}
+
+	if h.opts.Level == nil {
+		h.opts.Level = slog.LevelInfo
+	}
+
+	return h
+}
+
+func (p *PrettyPrintHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return level >= p.opts.Level.Level()
+}
+
+func (p *PrettyPrintHandler) Handle(ctx context.Context, r slog.Record) error {
+	buf := bytes.NewBuffer(make([]byte, 0, 1024))
+
+	if !r.Time.IsZero() {
+		fmt.Fprintf(buf, "%s ", r.Time.Format(time.RFC822Z))
+	}
+
+	var levelPrintFunc func(w io.Writer, format string, a ...interface{})
+
+	switch r.Level {
+	case slog.LevelDebug:
+		levelPrintFunc = color.New(color.FgCyan).FprintfFunc()
+	case slog.LevelWarn:
+		levelPrintFunc = color.New(color.FgYellow).FprintfFunc()
+	case slog.LevelError:
+		levelPrintFunc = color.New(color.FgRed).FprintfFunc()
+	default:
+		levelPrintFunc = color.New(color.Reset).FprintfFunc()
+	}
+
+	levelPrintFunc(buf, "%s", r.Level)
+	fmt.Fprint(buf, ": ")
+
+	fmt.Fprintf(buf, "%s ", r.Message)
+
+	attrs := map[string]string{}
+
+	r.Attrs(func(a slog.Attr) bool {
+		attrs[a.Key] = a.Value.String()
+		return true
+	})
+
+	if len(attrs) != 0 {
+		json := json.NewEncoder(buf)
+
+		json.Encode(attrs)
+	} else {
+		fmt.Fprint(buf, "\n")
+	}
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	_, err := p.out.Write(buf.Bytes())
+	return err
+}
+
+func (p *PrettyPrintHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return p
+}
+
+func (p *PrettyPrintHandler) WithGroup(name string) slog.Handler {
+	return p
+}
 
 var Logger *slog.Logger
 
@@ -29,28 +119,7 @@ func getSlogLevelFromString() (slog.Level, error) {
 }
 
 func InitLogger() {
-	err := config.ReadConfig()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
 	output := viper.GetString("logging.output")
-	var logOutput io.Writer
-
-	switch output {
-	case "stdout":
-		logOutput = os.Stdout
-	case "stderr":
-		logOutput = os.Stderr
-	default:
-		file, err := os.OpenFile(output, os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "ERROR: Unable to open log file")
-			os.Exit(1)
-		}
-
-		logOutput = file
-	}
 
 	level, err := getSlogLevelFromString()
 	if err != nil {
@@ -58,11 +127,31 @@ func InitLogger() {
 		os.Exit(1)
 	}
 
-	opts := &slog.HandlerOptions{
-		Level: level,
+	var logHandler slog.Handler
+
+	switch output {
+	case "stdout":
+		logHandler = NewPrettyPrintHandler(os.Stdout, &PrettyPrintHandlerOptions{
+			Level: level,
+		})
+	case "stderr":
+		logHandler = NewPrettyPrintHandler(os.Stderr, &PrettyPrintHandlerOptions{
+			Level: level,
+		})
+	default:
+		file, err := os.OpenFile(output, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "ERROR: Unable to open log file")
+			os.Exit(1)
+		}
+		opts := &slog.HandlerOptions{
+			Level: level,
+		}
+
+		logHandler = slog.NewTextHandler(file, opts)
 	}
 
-	Logger = slog.New(slog.NewTextHandler(logOutput, opts))
+	Logger = slog.New(logHandler)
 }
 
 func LogCritical(message string, args ...any) {
